344f7d2ef925698158a2d9877487f3e390198676
aagarwala <aditya@agarwala.us>
2019-07-03 16:12:13 -0700

    Initial commit

---
+++ frontend/util/session_api_util.js
@@ -0,0 +1,27 @@
+export const signup = user => (
+    $.ajax({
+        method: 'POST',
+        url: '/api/session',
+        data: { user }
+    })
+);
+
+export const login = user => (
+    $.ajax({
+        method: 'POST',
+        url: '/api/session',
+        data: { user }
+    })
+);
+
+export const logout = () => (
+    $.ajax({
+        method: 'DELETE',
+        url: '/api/session'
+    })
+);
+
+window.signup = signup;
+window.login = login;
+window.logout = logout;
+
